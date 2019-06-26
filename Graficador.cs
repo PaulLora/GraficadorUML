2d6b5c057713741d0986b2958ab72d9465126912
PaulLora <PaulLora@users.noreply.github.com>
2019-06-26 16:55:59 -0500

    first commit

---
+++ Graficador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graficador_de_UML
+{
+    public partial class Graficador : Form
+    {
+        public Graficador(String usuario)
+        {
+            InitializeComponent();
+            lblBienvenida.Text = "Bienvenid@\n"+usuario;
+        }
+    }
+}
